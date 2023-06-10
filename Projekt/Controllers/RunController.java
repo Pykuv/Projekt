db744b80caa6dd3904794b81e85b69bcef63b775
Dawid <daw.cyb14@gmail.com>
2023-06-10 16:58:08 +0200

    utworzenie kontrolera z klasa person

--- Projekt/Controllers/RunController.java
+++ Projekt/Controllers/RunController.java
@@ -1,6 +1,4 @@
 package controllers;
-
-import models.Animal;
 import models.BasicData;
 import models.Person;
 import java.util.Arrays;
@@ -14,40 +12,17 @@ public class RunController extends BasicData{
      * Class to run program
      */
     public void run() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj przyczyne choroby? ");
-        String decision = String.valueOf(scanner.nextLine());
+        //Scanner scanner = new Scanner(System.in);
+        Person person = new Person();
+        System.out.println("Wpisz imię pacjenta");
+        person.setName(String.valueOf(scanner.nextLine()));
+        System.out.gender("Podaj płeć");
+        person.work = String.valueOf(scanner.nextLine());
+        System.out.println("Podaj wiek ");
+        person.setAge(Integer.parseInt(scanner.nextLine()));
+        System.out.println(Arrays.toString(person.PersonData()));
 
-        switch (decision) {
-            case "human" -> {
-                Person person = new Person();
-                person.welcome(decision);
-                person.breakLine();
-                System.out.println("Podaj imię dla człowieka");
-                person.setName(String.valueOf(scanner.nextLine()));
-                System.out.gender("Podaj płeć");
-                person.work = String.valueOf(scanner.nextLine());
-                System.out.println("Podaj wiek człowieka");
-                person.setAge(Integer.parseInt(scanner.nextLine()));
-                System.out.println(Arrays.toString(person.PersonData()));
-            }
-            case "animal" -> {
-                Animal animal = new Animal();
-                animal.welcome(decision);
-                animal.breakLine();
-                System.out.println("Podaj imię dla zwierzaka");
-                animal.setName(String.valueOf(scanner.nextLine()));
-                System.out.println("Podaj gatunek zwierzęcia");
-                animal.setType(String.valueOf(scanner.nextLine()));
-                System.out.println("Podaj wiek zwierzęcia");
-                animal.setAge(Integer.parseInt(scanner.nextLine()));
-                System.out.println(Arrays.toString(animal.AnimalData()));
-            }
-            default -> {
-                System.out.println("Błędny wybór");
-                this.run();
-            }
-        }
+            
         this.breakLine();
         System.out.println("Do zobaczenia!");
     }
